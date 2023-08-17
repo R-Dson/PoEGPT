6b644877672905b4967994f5a5b0174107a30fbc
R-Dson <robindavidsson@outlook.com>
2023-08-18 00:37:59 +0200

    script to only grab text

--- scrape_only_text.py
+++ scrape_only_text.py
@@ -5,44 +5,33 @@ from bs4 import BeautifulSoup, SoupStrainer
 import re
 #from trafilatura import fetch_url, extract
 #from trafilatura.utils import sanitize, trim
+import os
+from bs4 import BeautifulSoup
+import re
 from urllib.parse import urljoin
 import json
-from torch import cuda, bfloat16
 import time
 import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from transformers import pipeline, AutoTokenizer, AutoModel, LlamaForCausalLM, BitsAndBytesConfig, BartForCausalLM
 from transformers.utils import logging
 import asyncio
-import random
-
-import concurrent.futures
 
 logging.set_verbosity_error()
 
-MODEL_PATH = 'lmsys/vicuna-7b-v1.5-16k'
-DATA_PATH = 'data/text.json'
-URL_PATH = 'data/urls.json'
-MODEL_PATH = 'kabita-choudhary/finetuned-bart-for-conversation-summary' #works well and is small
+DATA_PATH = 'data/text_unprocessed.json'
+URL_PATH = 'data/urls_unprocessed.json'
 
 BLACK_LIST_LANG = [ '/cn/', '/kr/', '/ru/', '/jp/', '/tw/', '/po/', '/th/', '/fr/', '/de/', '/es/',  '/br/', '/cn', '/kr', '/ru', '/jp', '/tw', '/th', '/fr', '/de', '/es',  '/br', '/trade/search', '/privacy', '/economy', 'facebook', 'instagram', 'youtube', 'twitch', 'discord', 'reddit', 'tiktok', 'linkedin', 'pinterest', 'tumblr', 'vimeo', 'snapchat' ]
 alpha = re.compile(r'[a-zA-Z]')
 counter = 0
 
-MAXLEN = 1024#4096
 lock = threading.Lock()
 black_list_url = set()
 todo_list_url = set()
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-device_map = "auto"
-num_models = 4
-
-pipelines = [pipeline("summarization", tokenizer=tokenizer, model=MODEL_PATH, device_map=device_map) for _ in range(num_models)]
-
 class Crawler:
-
+    
     def __init__(self, url: str, max_depth: int):
 
         self.url = url
@@ -50,28 +39,15 @@ class Crawler:
         self.max_depth = max_depth
         self.depth = 0
 
-        options = Options()
-        options.headless = True
-        self.driver = webdriver.Chrome(options=options)
-        self.driver.implicitly_wait(1)
+        options = Options() 
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1280,1280")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--enable-javascript")
 
-    def generate_instruction(self, url_text):
-        generator = random.choice(pipelines)
-        if len(url_text) > MAXLEN:
-            chunks = len(url_text)//MAXLEN
-            url_text_chunks = [ url_text[i:i+MAXLEN] for i in range(0, MAXLEN*chunks+1, MAXLEN) ]
-            tmp_MAXLEN = MAXLEN//(len(url_text_chunks))
-            if tmp_MAXLEN < 6:
-                tmp_MAXLEN = 6
-            url_text_chunks = generator(url_text_chunks, min_length=5, max_length=tmp_MAXLEN)
-            url_text_chunks = " ".join([ chunk['summary_text'] for chunk in url_text_chunks ])
-            output_instruction = url_text_chunks[:MAXLEN]
-        else:
-            output_instruction = generator(url_text, max_length=MAXLEN)
-            #self.generator(prompt, max_length=MAXLEN, return_full_text=False)#(f'Instruction based on "${url_text}", a Path of Exile I recommend is ', max_length=MAXLEN, return_full_text=False)
-            output_instruction = output_instruction[0]['summary_text']#['generated_text']
-        output_instruction = f'Provide more information about the following in Path of Exile: {output_instruction}'
-        return output_instruction
+        self.driver = webdriver.Chrome(options=options)
+        
+        self.driver.implicitly_wait(2)
 
     def crawl(self, url: str, depth=0):
         global counter
@@ -80,27 +56,23 @@ class Crawler:
 
         if depth > max_depth:
             return
-
+        
         with lock:
             if url in black_list_url or url in todo_list_url or any([lang in url for lang in BLACK_LIST_LANG]):
                 return
-
+        
         if any(item in url for item in ['pathofexile.com', base_url]):
             pass
         else:
             return
-        #if 'http://pathofexile.com' not in url and 'https://www.pathofexile.com' not in url and base_url not in url:
-        #    return
-
-        #if (base_url or ) not in url:
-        #    return
 
         self.driver.get(url)
+        time.sleep(0.25)
         html = self.driver.page_source
 
         if html is None:
             return
-
+        
         with lock: # probably not needed
             todo_list_url.add(url)
 
@@ -115,29 +87,69 @@ class Crawler:
                 i += 1
                 to_visit.append(a)
                 self.crawl(a, depth=depth+1)
+        
+        soup_extra = BeautifulSoup(html, 'html.parser')
+        remove_list = { 'Payment', 'Buy ', 'preferences', 'partners', 'partner', 'paypal' ,'skrill', 'crypto', 'facebook', 'instagram', 'privacy', 'policy', 'creative', 'commons', 'disclaimers', 'patreon', 'twitter', 'youtube', 'twitch', 'discord', 'reddit', 'tiktok', 'linkedin', 'pinterest', 'tumblr', 'vimeo', 'snapchat', 'whatsapp', 'imprint', 'impressum', 'contact', 'cookies', 'privacy', 'policy', 'legal', 'data', 'protection', 'sitemap', 'accessibility', 'accessable', 'access', 'help', 'faq', 'support', 'donate', 'donation', 'donations', 'report', 'reports', 'advertising', 'cookies', 'cookie'}
+        time_save = time.time()
+        # Remove header
+        try:
+            header = soup_extra.find('header')
+            header.decompose()
+        except:
+            pass
 
-        url_text = soup.get_text(strip=True)
+        try:
+            footer = soup_extra.find('footer')
+            footer.decompose()
+        except:
+            pass
 
+        try:
+            nav = soup_extra.find('nav')
+            for n in nav:
+                n.decompose()
+        except:
+            pass
+
+        try:
+            sidebars = soup_extra.find_all('aside')
+            for sb in sidebars:
+                sb.decompose()
+        except:
+            pass
+
+        for tag in soup_extra.find_all('a'):
+            if any(x in tag.get_text().lower() for x in remove_list):
+                tag.decompose()
+
+        url_text = soup_extra.get_text('\n', strip=True)
+        
         if url_text is None:
             return
 
+        url_text = ' '.join([ x for x in TextBlob(url_text).raw_sentences if ('' if any(y in x.lower() for y in remove_list) else x) ])
+        
         url_text = url_text.replace('\n', ' ')
         url_text = re.sub(r'[^\sa-zA-Z0-9\._-]', '', url_text)
         url_text = re.sub(r'([A-Z])', r' \1', url_text)
         url_text = re.sub('\s+', ' ', url_text)
         url_text = re.sub(r'[^\w\s]', '', url_text).strip()
 
+        url_text = url_text.replace(' Po E ', ' PoE ')
+        url_text = url_text.replace(' Po E.', ' PoE.')
+        url_text = url_text.replace(' Po E,', ' PoE,')
+        url_text = url_text.replace('Po E ', 'PoE ')
+
+        process_delta = time.time() - time_save
+        if url_text == '':
+            return
+        
         if not alpha.match(url_text):
             return
 
-        start_generate = time.time()
-        output_instruction = self.generate_instruction(url_text)
-        denerate_delta = time.time() - start_generate
-
         tmpdict = {
-            "instruction": output_instruction,
-            "input": "",
-            "output": url_text
+            "output": url_text,
+            "processed": False,
         }
 
         time_save = time.time()
@@ -145,21 +157,28 @@ class Crawler:
             self.save_to_file(tmpdict)
             save_delta = time.time() - time_save
 
-            black_list_url.add(url)
-            todo_list_url.remove(url)
+            try:
+                black_list_url.add(url)
+            except:
+                pass
+
+            try:
+                todo_list_url.remove(url)
+            except:
+                pass
 
             time_save_url = time.time()
             self.save_urls()
             save_url_delta = time.time() - time_save_url
             counter += 1
 
-            print(f'Finished processing: Counter: {counter}. Depth: {depth} of {max_depth}. {url}. Time: {denerate_delta:.2f}s. Save time: {save_delta:.2f}s. Save url time: {save_url_delta:.2f}s.')
-
+            print(f'Finished processing: Counter: {counter}. Depth: {depth} of {max_depth}. {url}. Save time: {save_delta:.2f}s. Save url time: {save_url_delta:.2f}s. Process time: {process_delta:.2f}s.')
+        
     def save_to_file(self, dictionary):
             with open(DATA_PATH, 'rb+') as filehandle:
                 filehandle.seek(-1, os.SEEK_END)
                 filehandle.truncate()
-
+            
             with open(DATA_PATH, 'a') as f:
                 f.seek(0, 2)
                 if f.tell() == 0:
@@ -181,18 +200,19 @@ def load_urls():
         black_list_url = set(json.load(f))
         return black_list_url
     except:
-        print("No URL file found")
+        print("No URL file found")  
         return set()
-
+             
 urls = {
-    'https://poedb.tw/us' : 3,
-    'https://poebuilds.cc' : 3,
-    'https://poe-vault.com' : 3,
-    'https://maxroll.gg/poe' : 3,
-    'https://poe.ninja' : 2,
-    'https://pathofexile.com/forum/view-thread/3409617' : 1,
-    'https://old.reddit.com/r/PathOfExileBuilds': 2,
-    'https://poewiki.net' : 3
+    'https://poedb.tw/us' : 5,
+    'https://poebuilds.cc' : 5,
+    'https://poe-vault.com' : 5,
+    'https://maxroll.gg/poe' : 5,
+    'http://www.vhpg.com': 5,
+    'https://poe.ninja' : 3,
+    'https://pathofexile.com/forum/view-thread/3409617' : 3,
+    'https://old.reddit.com/r/PathOfExileBuilds': 4,
+    'https://poewiki.net' : 5
 }
 
 async def crawl_thread(c: Crawler, url: str):
@@ -212,15 +232,7 @@ async def main():
     await asyncio.gather(*tasks)
     executor.shutdown()
     return
-    tasks = []
-    for url, max_depth in urls.items():
-        c = Crawler(url, max_depth)
-        crawlers.append(c)
-        await loop.run_in_executor(executor, crawl_thread)
-        #task = loop.create_task(crawl_thread(c, url))
-        #tasks.append(task)
-    await asyncio.gather(*tasks)
-
+    
 from concurrent.futures import ThreadPoolExecutor
 if __name__ == "__main__":
     black_list_url = load_urls()
@@ -229,4 +241,4 @@ if __name__ == "__main__":
 
     for c in crawlers:
         c.driver.close()
-
+    
